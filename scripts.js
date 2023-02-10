eda2b8d3ad05d3a18991b1fa9496e2de48a53ebc
brett-jan <53590039+brett-jan@users.noreply.github.com>
2023-02-09 21:22:17 -0700

    updated

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+window.onload = function() {
+    weatherBalloon();
+}
+
 function weatherBalloon() {
     var base_url = "https://api.openweathermap.org/data/3.0/onecall?lat="
     var lat = "51.057813435595875"
@@ -10,7 +14,7 @@ function weatherBalloon() {
     .then(function(resp) { return resp.json() }) // Convert data to json
     .then(function(data) {
         console.log(data);
-        currentConditions(data); // Call drawWeather
+        currentConditions(data);
         tomorrowMorning(data);
     })
     .catch(function() {
@@ -18,12 +22,18 @@ function weatherBalloon() {
     });
 }
 
-window.onload = function() {
-    weatherBalloon();
-}
-
 function currentConditions( d ) {
-    document.getElementById('temp').innerHTML = Math.round(parseFloat(d.current.temp)) + '&deg;';
+    document.getElementById('current_temp').innerHTML = Math.round(parseFloat(d.current.temp)) + '&deg;';
+    document.getElementById('current_feelslike').innerHTML = Math.round(parseFloat(d.current.feels_like)) + '&deg;';
+    document.getElementById('current_windspeed').innerHTML = Math.round(parseFloat(d.current.wind_speed) * 3.6) + ' km/h';
+    const currentTime = new Date();
+    if (currentTime.getHours() < 10) {
+        document.getElementById('current_winddirection').innerHTML = windDirectionMorning(d.current.wind_deg);
+    } else if (currentTime.getHours() < 19) {
+        document.getElementById('current_winddirection').innerHTML = windDirectionEvening(d.current.wind_deg);
+    } else {
+        document.getElementById('current_winddirection').innerHTML = windDirection(d.current.wind_deg);
+    }
 }
 
 function tomorrowMorning( d ) {
@@ -35,8 +45,6 @@ function tomorrowMorning( d ) {
     tomorrow7AM.setSeconds(0)
     tomorrow7AM.setMilliseconds(0)
     const tomorrow7AM_ts = Math.floor(tomorrow7AM.getTime() / 1000);
-    document.getElementById('currentTime').innerHTML = currentTime;
-    document.getElementById('tomorrow7AM').innerHTML = tomorrow7AM_ts;
 
     for (i=0; i < d.hourly.length; i++){
         if (d.hourly[i].dt === tomorrow7AM_ts){
@@ -45,5 +53,8 @@ function tomorrowMorning( d ) {
             console.log(d.hourly[i].dt);
         }
     }
-    }
-    function degToCard(value) { value = parseFloat(value); if (value <= 11.25) return 'N'; value -= 11.25; var allDirections = ['NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW', 'N']; var dIndex = parseInt(value/22.5); return allDirections[dIndex] ? allDirections[dIndex] : 'N'; }
+}
+
+    function windDirectionMorning(value) { value = parseFloat(value); if (value <= 11.25) return 'N'; value -= 11.25; var allDirections = ['NNE', 'NE (headwind)', 'ENE (headwind)', 'E (headwind)', 'ESE (headwind)', 'SE (headwind)', 'SSE', 'S', 'SSW', 'SW (tailwind)', 'WSW (tailwind)', 'W (tailwind)', 'WNW (tailwind)', 'NW (tailwind)', 'NNW', 'N']; var dIndex = parseInt(value/22.5); return allDirections[dIndex] ? allDirections[dIndex] : 'N'; }
+    function windDirectionEvening(value) { value = parseFloat(value); if (value <= 11.25) return 'N'; value -= 11.25; var allDirections = ['NNE', 'NE (tailwind)', 'ENE (tailwind)', 'E (tailwind)', 'ESE (tailwind)', 'SE (tailwind)', 'SSE', 'S', 'SSW', 'SW (headwind)', 'WSW (headwind)', 'W (headwind)', 'WNW (headwind)', 'NW (headwind)', 'NNW', 'N']; var dIndex = parseInt(value/22.5); return allDirections[dIndex] ? allDirections[dIndex] : 'N'; }
+    function windDirection(value) { value = parseFloat(value); if (value <= 11.25) return 'N'; value -= 11.25; var allDirections = ['NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW', 'N']; var dIndex = parseInt(value/22.5); return allDirections[dIndex] ? allDirections[dIndex] : 'N'; }
